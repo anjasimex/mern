ce633aefb2956b9d409798f323dd1fb9197aaca7
anjas <anjasmuslim05@gmail.com>
2021-10-08 07:52:35 +0700

    new init mern

---
+++ backend/data/notes.js
@@ -0,0 +1,17 @@
+const notes = [
+    {
+        _id: "1",
+        title: "anjas superhero",
+        content: "kekuatan super",
+        category: "Blog"
+    },
+    {
+        _id: "elang pasir",
+        title: "elang pasir juara",
+        content: "juara liga tarkam",
+        category: "News"
+
+    }
+]
+
+module.exports = notes;
